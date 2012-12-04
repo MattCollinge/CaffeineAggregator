72ffb447faeaf6c0d381fcfe076071da85316859
Matt Collinge <matt.collinge@gmail.com>
2012-12-04 18:49:47 +0000

    Matt: added KO...

--- static/js/monitor.js
+++ static/js/monitor.js
@@ -4,6 +4,7 @@
 
     var conn = io.connect('http://localhost:8083');
     var sessionId;
+    var koModel;
 
     function send(){
         var val = document.getElementById('text').value;
@@ -20,6 +21,7 @@
 
     conn.on('purchase', function (obj) {
         message('Served ' +  obj.drink + ' at '+ obj.stamp);
+        koModel.firstName(JSON.stringify(obj));
     });
 
     conn.on('status', function (obj) {
@@ -32,10 +34,39 @@
     });
 
     conn.on('dataSeries', function(data){
+        setupKO(data);
         renderGraph(data);
     });
 
 
+    //Knockout Model
+    function AppViewModel(data) {
+        this.timeSeries = data;
+        this.lastName = "Bertington";
+        this.firstName = ko.observable("Bob").extend({logChange: "first name"});
+        this.fullName = ko.computed(function() {
+                                        return this.firstName() + " " + this.lastName();
+                                    }, this);    
+    }
+
+    function setupKO(data){
+         //Add extender to subscribe to observable
+         ko.extenders.logChange = function(target, option) {
+                                        target.subscribe(function(newValue) {
+                                           console.log(option + ": " + newValue);
+                                        });
+                                        return target;
+                                    };
+         //Create ViewModel
+         koModel = new AppViewModel(data);
+
+         // Activates knockout.js for DOM
+        //ko.applyBindings(koModel);
+    }
+
+    
+
+   
     //Rickshaw.js - Charting
     function renderGraph(data){
         var graph = new Rickshaw.Graph( {
